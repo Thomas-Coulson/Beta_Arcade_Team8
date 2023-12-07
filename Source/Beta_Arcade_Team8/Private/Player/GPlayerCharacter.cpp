d425e9f42dbaaaa92b92995f736ca0883567be8c
Thomas Coulson <78150031+Thomas-Coulson@users.noreply.github.com>
2023-12-07 13:58:42 +0000

    Wall running (#32)
    * Added Basic Wall Run Functionality
    
    - Added ability to wall run
    - player can jump off walls (this needs refining more though)
    - need to find a way to lock camera movement
    
    * Improvements to wallrun and climb movement
    
    * Added Tag Check to Climbup
    
    - Climb Up now checks for NoClimb Tag on objects
    
    * Tag actually works now
    
    * NoClimb Tag checks the correct actor now
    
    * Fixed Climb up code
    
    * cleaned up comments
    
    * setup up basics for wall run timers
    
    * Level timer now restarts when using "Start Timer"
    
    * Wall run now stops after a set amount of time
    
    - Can no longer wall run forever
    - Should all work with everything else
    
    * Improved Wall Running
    
    - Can only wallrun / climb once per wall before landing

--- Source/Beta_Arcade_Team8/Private/Player/GPlayerCharacter.cpp
+++ Source/Beta_Arcade_Team8/Private/Player/GPlayerCharacter.cpp
@@ -99,9 +99,7 @@ void AGPlayerCharacter::Tick(float DeltaTime)
 	{
 		//print("Is Falling");
 		//TomC - Setup left and right Line Traces for Wall Running
-		FHitResult RightHit;
-		FHitResult LeftHit;
-		FHitResult FrontHit;
+		
 
 		FVector TraceStart = GetActorLocation();
 		FVector FrontTraceStart = FVector(GetActorLocation().X, GetActorLocation().Y, GetActorLocation().Z - FrontTraceOffset);
@@ -124,7 +122,7 @@ void AGPlayerCharacter::Tick(float DeltaTime)
 		{
 			if (GetWorld()->LineTraceSingleByChannel(FrontHit, FrontTraceStart, FrontTraceEnd, FrontTraceChannelProperty, FrontQueryParams))
 			{
-				if (!FrontHit.GetActor()->ActorHasTag("NoClimb"))
+				if (!FrontHit.GetActor()->ActorHasTag("NoClimb") && FrontHit.GetActor() != PreviousWall)
 				{
 					ClimbingFront = true;
 					if (CurrentClimbs == 0)
@@ -151,7 +149,7 @@ void AGPlayerCharacter::Tick(float DeltaTime)
 		{
 			if (GetWorld()->LineTraceSingleByChannel(RightHit, TraceStart, RightTraceEnd, RightTraceChannelProperty, RightQueryParams))
 			{
-				if (!JumpingOffWallRight && CurrentClimbs < MaxClimbs)
+				if (!JumpingOffWallRight && CurrentClimbs < MaxClimbs && RightHit.GetActor() != PreviousWall)
 				{
 					//Touching Right wall in the air (and not jumping off)
 					if (!RunningOnRight)
@@ -181,7 +179,7 @@ void AGPlayerCharacter::Tick(float DeltaTime)
 		{
 			if (GetWorld()->LineTraceSingleByChannel(LeftHit, TraceStart, LeftTraceEnd, LeftTraceChannelProperty, LeftQueryParams))
 			{
-				if (!JumpingOffWallLeft && CurrentClimbs < MaxClimbs)
+				if (!JumpingOffWallLeft && CurrentClimbs < MaxClimbs && LeftHit.GetActor() != PreviousWall)
 				{
 					//Touching Right wall in the air (and not jumping off)
 					if (!RunningOnLeft)
@@ -215,6 +213,7 @@ void AGPlayerCharacter::Tick(float DeltaTime)
 	}
 	else
 	{
+		PreviousWall = NULL;
 		CurrentClimbs = 0;
 		HasRunOnRight = false;
 		wallrunStopped = false;
